b26a8106e82e4479ab58b51643598cb3b9306901
SAEKI Yoshiyasu <laclef.yoshiyasu@gmail.com>
2012-05-19 21:28:01 +0900

    added function for writing data into udp socket into background

---
+++ background.js
@@ -0,0 +1,23 @@
+
+var udp_socket_writer = function(string, host, port) {
+    var string2ArrayBufferAndWrite = function(str) {  // http://goo.gl/1eJnn
+        var bb = new WebKitBlobBuilder();
+        bb.append(str);
+        var f = new FileReader();
+        f.onload = function(e) {
+            var data = e.target.result;
+            chrome.experimental.socket.create(
+                'udp', {},
+                function(socketInfo) {
+                    chrome.experimental.socket.connect(
+                        socketInfo.socketId, host, port,
+                        function(result) {
+                            chrome.experimental.socket.write(socketInfo.socketId, data,
+                                                             function(writeInfo){ console.log(writeInfo); })
+                        });
+                });
+        };
+        f.readAsArrayBuffer(bb.getBlob());
+    };
+    string2ArrayBufferAndWrite(string);
+}
